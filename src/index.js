f3f8d5ffd6d94ddf956f52d1bda365b3ecef45cd
Anushka Dubey <anushkadubey388@gmail.com>
2023-07-27 18:37:35 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -2,10 +2,17 @@ var React = require("react");
 var ReactDOM = require("react-dom");
 
 ReactDOM.render(
+    <>
+    <h1>Anushka's Top Netflix Picks</h1>
+    <p>Here comes the list of my top 5 favourite Netflix series  </p>
+    <ol>
+        <li>Never have I ever</li>
+        <li>The Silent Sea</li>
+        <li>The Glory</li>
+        <li>Decendants of the Sun</li>
+        <li>The Guardian: Lonely and the Great God</li>
+    </ol>
+    </>,
+    document.getElementById("root")
 
-[
-<h1>Hello World</h1>,
-<p>This is anushka</p>,
-<p>Lets start the day</p>],
-
-document.getElementById("root"));
+);

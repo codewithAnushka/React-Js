f5c45cde37979d11aca38bbcf20188cff7bfac4e
Anushka Dubey <anushkadubey388@gmail.com>
2023-07-27 17:31:51 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ var React = require("react");
 var ReactDOM = require("react-dom");
 
 ReactDOM.render(
-<div>
-<h1>Hello World</h1>, 
-</div>,
+
+[
+<h1>Hello World</h1>,
+<p>This is anushka</p>,
+<p>Lets start the day</p>],
+
 document.getElementById("root"));
